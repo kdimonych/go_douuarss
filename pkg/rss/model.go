3dea100f1df11742d25c54e690b5ed1c6104e54a
Dmitry Krasnyansky <k.dimonych@gmail.com>
2025-06-09 19:41:19 +0300

    fix: fixed xml model. The structural test for xml was implemented

--- pkg/rss/model.go
+++ pkg/rss/model.go
@@ -11,26 +11,22 @@ type Xml struct {
 }
 
 type Channel struct {
-	Title         string `xml:"channel"`       // Title of the RSS channel
-	Link          string `xml:"link"`          // Link to the RSS channel
-	Description   string `xml:"description"`   // Description of the RSS channel
-	Language      string `xml:"language"`      // Language of the RSS channel
-	LastBuildDate string `xml:"lastBuildDate"` // Last build date of the RSS channel
-	Items         []Item `xml:"item"`          // List of items in the RSS channel
+	Title         string       `xml:"title"`         // Title of the RSS channel
+	Link          string       `xml:"link"`          // Link to the RSS channel
+	Description   string       `xml:"description"`   // Description of the RSS channel
+	Language      string       `xml:"language"`      // Language of the RSS channel
+	LastBuildDate RFC1123ZDate `xml:"lastBuildDate"` // Last build date of the RSS channel
+	Items         []Item       `xml:"item"`          // List of items in the RSS channel
 }
 
 type Item struct {
-	Title       string    `xml:"title"`       // Title of the article
-	Link        string    `xml:"link"`        // Link to the article
-	Description string    `xml:"description"` // Description of the article
-	PubDate     PubDate   `xml:"pubDate"`     // Publication date of the article
-	Creators    []Creator `xml:"creator"`     // List of creators of the article
+	Title       string       `xml:"title"`       // Title of the article
+	Link        string       `xml:"link"`        // Link to the article
+	Description string       `xml:"description"` // Description of the article
+	PubDate     RFC1123ZDate `xml:"pubDate"`     // Publication date of the article
+	Creator     string       `xml:"creator"`     // List of creators of the article
 }
 
-type PubDate struct {
+type RFC1123ZDate struct {
 	time.Time
-}
-
-type Creator struct {
-	Name string `xml:"__text"` // Name of the creator
 }
